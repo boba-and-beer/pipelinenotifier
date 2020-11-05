9e5b0b82afa3c6e37a5b295ef8bdc5a8943ae7b8
Jacky <jacky2wong@gmail.com>
2020-11-05 15:31:35 +1100

    Update setup

---
+++ pipelinenotifier/README.md
@@ -0,0 +1,4 @@
+All different channel types go here. 
+
+If a specific channel type is desired, please create an issue.
+
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jacky-tools", # Replace with your own username
-    version="0.0.1",
+    name="pipeline-notifier", # Replace with your own username
+    version="0.0.2",
     author="Jacky Wong",
     author_email="jacky.wong@vctr.ai",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/boba-and-beer/pipelinenotifier",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
